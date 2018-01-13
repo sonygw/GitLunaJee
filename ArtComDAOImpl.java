55124ab2c11a644975082ea928ec986763da1783
DannyTheMicrobiologist <33761151+DannyTheMicrobiologist@users.noreply.github.com>
2018-01-13 09:47:52 +0100

    Modification implémentations
    ArtComDAOImpl et ClientDAOImpl

--- ArtComDAOImpl.java
+++ ArtComDAOImpl.java
@@ -40,8 +40,6 @@ public class ArtComDAOImpl implements ArtComDAO {
 	@Override
 	public ArrayList<ArtCom> SelectAllArtComFromCommande(int idCommande) {
 		
-			//result = state.executeQuery("Select * from artCom where idCommande =" + idCommande);
-
 		@SuppressWarnings("unchecked")
 		TypedQuery<ArtCom> result = sessionFactory.getCurrentSession().createQuery("from artCom where idCommande =" +idCommande);
 		return (ArrayList<ArtCom>) result.getResultList();
@@ -50,65 +48,56 @@ public class ArtComDAOImpl implements ArtComDAO {
 
 	@Override
 	public boolean DeleteArtComFromCommande(ArtCom obj) {
-
-		boolean result = false;
 		
 		sessionFactory.getCurrentSession().delete(obj);
-		result=true;
 		
-		return result;
+		return true;
 	}
 
 	@SuppressWarnings("rawtypes")
 	@Override
 	public boolean UpdateArtComFromCommande(ArtCom obj, int idArtCom) {
 
-		boolean result = false;
-
 		Query query = sessionFactory.getCurrentSession().createQuery("Update artCom SET idCommande = " + obj.getIdArtCom() + ", idArticle = "
 				+ obj.getIdArticle() + "," + obj.getQuantite() + " where idArtCom = " + idArtCom);
-			result = true;
+			
 
 			query.executeUpdate();
-		return result;
+		return true;
 	}
 
 	@SuppressWarnings("rawtypes")
 	@Override
 	public boolean CreateArtComFromCommande(ArtCom obj) {
-		boolean result = false;
 
 		Query query = sessionFactory.getCurrentSession().createQuery("insert into artCom values (null, " + obj.getIdCommande() + "," + obj.getIdArticle()
 		+ "," + obj.getQuantite() + ")");
-			result = true;
 			query.executeUpdate();
 
-		return result;
+		return true;
 	}
 
 	@SuppressWarnings("rawtypes")
 	@Override
 	public boolean DeleteArtComFromIdCommande(int id) {
 
-		boolean result = false;
+		
 		
 		Query query = sessionFactory.getCurrentSession().createQuery("Delete from artCom where idCommande =" + id);
-		result = true;
+		
 		query.executeUpdate();
 
-		return result;
+		return true;
 
 	}
 
 	@SuppressWarnings("rawtypes")
 	@Override
 	public boolean DeleteArtComFromIdArticle(int id) {
-		boolean result = false;
-		
 		
 		Query query = sessionFactory.getCurrentSession().createQuery("Delete from artCom where idArticle =" + id);
 		query.executeUpdate();
-		return result;
+		return true;
 	}
 
 }
