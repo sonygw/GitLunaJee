fa03083960f5df524abdf2ab7fc76e93954975bd
Lou <Lou@Lou-PC.home>
2018-01-14 19:11:26 +0100

    upd

--- LunaJEE/src/main/java/com/formation/DAO/UserDAOImpl.java
+++ LunaJEE/src/main/java/com/formation/DAO/UserDAOImpl.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.formation.model.User;
 
+@Repository
 public class UserDAOImpl implements UserDAO {
 
 	@Autowired
